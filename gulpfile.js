aa820ce23d81c17e7d1d5ff88336cafd97ee2f30
looading <946505808@qq.com>
2016-03-31 11:00:23 +0800

    create success

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+var browserSync = require('browser-sync')
+var reload = browserSync.reload
+
+var gulpChild = require('./gulp-child')
+var restart = gulpChild.restart
+
+var gulp = require('gulp')
+gulp.task('createChild', function() {
+	gulpChild.init({
+		bin : 'node',
+		main : ['app.js'],
+		stdout : function(data){
+			console.log('stdout : ' + data + '\n')
+		},
+		stderr : function(data){
+			console.log('stderr : ' + data + '\n')
+		},
+		closeCb : function(code){
+			console.log(121312)
+			console.log(`child process exited with code ${code}`)
+		}
+	})	
+})
+
+gulp.task('killChild', function() {
+	gulpChild.close()
+})
+
+gulp.task('proxy', function() {
+	browserSync.init({
+		proxy : 'http://localhost:3000',
+		port : 8888
+	})
+})
+
+gulp.task('watch', function() {
+	gulp.watch('*.js').on('change', restart)
+})
+
+gulp.task('default', ['createChild','proxy'])
